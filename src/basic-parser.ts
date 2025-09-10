60078127acc50bf9df066dc6b399debaa794497c
kamyaraman <kamya@raman-home.com>
2025-09-10 14:19:08 -0400

    feat: accept zod schema

--- src/basic-parser.ts
+++ src/basic-parser.ts
@@ -1,5 +1,16 @@
 import * as fs from "fs";
 import * as readline from "readline";
+import { z, ZodType } from "zod";
+
+// Define the schema. This is a Zod construct, not a TypeScript type.
+export const PersonRowSchema = z.tuple([z.string(), z.coerce.number()])
+                         .transform( tup => ({name: tup[0], age: tup[1]}))
+
+
+// Define the corresponding TypeScript type for the above schema. 
+// Mouse over it in VSCode to see what TypeScript has inferred!
+export type Person = z.infer<typeof PersonRowSchema>;
+
 
 /**
  * This is a JSDoc comment. Similar to JavaDoc, it documents a public-facing
@@ -12,9 +23,10 @@ import * as readline from "readline";
  * You shouldn't need to alter them.
  * 
  * @param path The path to the file being loaded.
+ * @param schema A Zod schema used to validate data 
  * @returns a "promise" to produce a 2-d array of cell values
  */
-export async function parseCSV(path: string): Promise<string[][]> {
+export async function parseCSV<T>(path: string, schema: ZodType<T>): Promise<T[]> {
   // This initial block of code reads from a file in Node.js. The "rl"
   // value can be iterated over in a "for" loop. 
   const fileStream = fs.createReadStream(path);
@@ -24,14 +36,15 @@ export async function parseCSV(path: string): Promise<string[][]> {
   });
   
   // Create an empty array to hold the results
-  let result = []
+  let result :  T[] = []
   
   // We add the "await" here because file I/O is asynchronous. 
   // We need to force TypeScript to _wait_ for a row before moving on. 
   // More on this in class soon!
   for await (const line of rl) {
     const values = line.split(",").map((v) => v.trim());
-    result.push(values)
+    const parsed = schema.parse(values); 
+    result.push(parsed);
   }
   return result
 }
